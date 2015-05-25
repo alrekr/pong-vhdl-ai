acfe02248e56d21b8a8d5be9c9a26b973d74fb0d
Malthe Høj-Sunesen <malthe.s@gmail.com>
2015-05-25 11:56:51 +0200

    Added an "active" clause to allwo for pausing AI

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ the 'AI' work. */
 #define BAT_COORDS  0x00000000  // should be addr to mem location/FPGA interface
 #define BALL_COORDS 0x00000000  // should be addr to mem location/FPGA interface
 char UART_OUT;
+char active = 'y'; //must be updated in while loop
 
 int main() {
     int16_t x1 = 0, x2 = 0, y1 = 0, y2 = 0;
@@ -38,74 +39,78 @@ int main() {
 #endif  // DEBUG
 
     while (1) {
-        a = 0;
-        b = 0;
-        x2 = x1;
-        y2 = y1;
+        if (char = 'y') {
+
+            a = 0;
+            b = 0;
+            x2 = x1;
+            y2 = y1;
 
 #ifdef PROD
-        x1 = (BALL_COORDS & 0xffc00) >> 10;
-        y1 = BALL_COORDS & 0x003ff;
-        y_pos = BAT_COORDS & 0x003ff;
+            x1 = (BALL_COORDS & 0xffc00) >> 10;
+            y1 = BALL_COORDS & 0x003ff;
+            y_pos = BAT_COORDS & 0x003ff;
 #endif  // PROD
 
 #ifdef DEBUG
-        if (i >= 25) {
-            break;
-        }
+            if (i >= 25) {
+                break;
+            }
 
-        x1 = x_arr[i];
-        y1 = y_arr[i];
-        cout << "x1, y1: " << x1 << ", " << y1 << endl;
-        cout << "x2, y2: " << x2 << ", " << y2 << endl;
-        i++;
+            x1 = x_arr[i];
+            y1 = y_arr[i];
+            cout << "x1, y1: " << x1 << ", " << y1 << endl;
+            cout << "x2, y2: " << x2 << ", " << y2 << endl;
+            i++;
 #endif  // DEBUG
 
-        a = (static_cast<float>(y2) - y1) / (x2 - x1);
-        b = static_cast<float>(y1 - a * x1);
-        /* 40 because of offset for center of bat (50)
-        and offset of ball (10) cancels a bit out */
-        y_intersect = static_cast<int>(a * x_intersect + b + 40) % 600;
+            a = (static_cast<float>(y2) - y1) / (x2 - x1);
+            b = static_cast<float>(y1 - a * x1);
+            /* 40 because of offset for center of bat (50)
+            and offset of ball (10) cancels a bit out */
+            y_intersect = static_cast<int>(a * x_intersect + b + 40) % 600;
+            if (y_intersect < 0) {
+                y_intersect += 600;
+            }
 
-        if (y_pos < y_intersect && y_pos > 50) {
+            if (y_pos < y_intersect && y_pos > 50) {
 
 #ifdef DEBUG
-            cout << "Ball position: (" << setw(3) << x1 << ", " << setw(3)
-                    << y1 << ")\tIntersect: " << y_intersect << "\ty_pos: "
-                    << y_pos << "\tMoving bat up" << endl;
-            y_pos += 10;
+                cout << "Ball position: (" << setw(3) << x1 << ", " << setw(3)
+                        << y1 << ")\tIntersect: " << y_intersect << "\ty_pos: "
+                        << y_pos << "\tMoving bat up" << endl;
+                        y_pos += 10;
 #endif  // DEBUG
 
 #ifdef PROD
-            UART_OUT = 'u';  // This might need be changed to what Pong expects
+                UART_OUT = 'u';  // This might need be changed to what Pong expects
 #endif  // PROD
 
-        } else if (y_pos > y_intersect  && y_pos < 750) {
+            } else if (y_pos > y_intersect  && y_pos < 750) {
 
 #ifdef DEBUG
-            cout << "Ball position: (" << setw(3) << x1 << ", " << setw(3)
-                    << y1 << ")\tIntersect: " << y_intersect << "\ty_pos: "
-                    << y_pos << "\tMoving bat down" << endl;
-            y_pos -= 10;
-
+                cout << "Ball position: (" << setw(3) << x1 << ", " << setw(3)
+                        << y1 << ")\tIntersect: " << y_intersect << "\ty_pos: "
+                        << y_pos << "\tMoving bat down" << endl;
+                        y_pos -= 10;
 #endif  // DEBUG
 
 #ifdef PROD
-            UART_OUT = 'd';  // This might need be changed to what Pong expects
+                UART_OUT = 'd';  // This might need be changed to what Pong expects
 #endif  // PROD
 
-        } else {
+            } else {
 
 #ifdef DEBUG
-            cout << "Ball position: (" << setw(3) << x1 << ", " << setw(3)
-                    << y1 << ")\tIntersect: " << y_intersect << "\ty_pos: "
-                    << y_pos << "\tNot moving bat" << endl;
+                cout << "Ball position: (" << setw(3) << x1 << ", " << setw(3)
+                        << y1 << ")\tIntersect: " << y_intersect << "\ty_pos: "
+                        << y_pos << "\tNot moving bat" << endl;
 #endif  // DEBUG
 
 #ifdef PROD
-            UART_OUT = 'n';  // This might need be changed to what Pong expects
+                UART_OUT = 'n';  // This might need be changed to what Pong expects
 #endif  // PROD
-
+            }
         }
     }
 }
